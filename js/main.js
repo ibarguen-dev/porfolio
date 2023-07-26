304ca9a474fe5f255ccb1f2ea831872720ae194f
juan esteban torres ibarguen <jt158161@gmail.com>
2023-07-26 17:22:43 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// gsap.from(".square",{z:5000,duration:1 ,}) 
+gsap.from(".test",{z:5000,duration:1 ,}) 
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.fromTo(".square",{z:0, duration: 1} , {           
+//     z: 5000,
+//     duration:8 ,
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".div1",     
+//         start: "top top",    
+//         scrub:1,
+//         toggleActions: "play none none none",       
+//         markers: true,
+//         pin: ".div1",
+//     }
+       
+// })
+
+// gsap.fromTo(".square2",{z:-1000, duration: 1} , {              
+//     z: 4000,
+//     duration:8 ,
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".div1",     
+//         start: "top top",    
+//         scrub:1,
+//         toggleActions: "play none none none",       
+//         markers: true,
+//         pin: ".div1",
+//     }       
+// })
+
+// gsap.fromTo(".square3",{z:-2000, duration: 1} , {              
+//     z: 3000,
+//     duration:8 ,
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".div1",     
+//         start: "top top",    
+//         scrub:1,
+//         toggleActions: "play none none none",       
+//         markers: true,
+//         pin: ".div1",
+//     }       
+// })
+
+// gsap.fromTo(".square4",{z:-3000, duration: 1} , {              
+//     z: 2000,
+//     duration:8 ,
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".div1",     
+//         start: "top top",    
+//         scrub:1,
+//         toggleActions: "play none none none",       
+//         markers: true,
+//         pin: ".div1",
+//     }       
+// })
+
+// gsap.fromTo(".square5",{z:-4000, duration: 1} , {              
+//     z: 1000,
+//     duration:8 ,
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".div1",     
+//         start: "top top",    
+//         scrub:1,
+//         toggleActions: "play none none none",      
+//         markers: true,
+//         pin: ".div1",
+//     }       
+// })
