b7b622bf4f3eaa2a9d4b28808e719342d74c8b9d
juan esteban torres ibarguen <jt158161@gmail.com>
2023-07-28 12:14:40 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,44 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".student", {
+    scrollTrigger: {
+        trigger:"#student",
+        start:"top 500px",
+        end:" top px",  
+        scrub:1,
+        // markers:true
+    }, 
+    ease:"none",
+    x: -1950,
+    duration:90
+});
+
+gsap.from(".cardStudent",{
+    scrollTrigger:{
+        trigger:"#student",
+        start:"top 500px",
+        end:" top 500px",  
+        scrub:1,
+        //markers:true
+    },
+    background:"red",
+    rotation:250
+})
+
+gsap.from(".card-port",{
+    scrollTrigger:{
+        trigger:"#Portafolio",
+        start:"top 500px",
+        end:" top 500px",
+        scrub:1,
+    },
+    background:"transparent"
+})
+
+gsap.from("#Arbol",{
+    scrollTrigger:{
+        trigger:"#Arbol",
+        // markers:true,
+        end:" top ", 
+        start:"top 100px",
+    }
+})
