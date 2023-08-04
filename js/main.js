395828e7d93dbb24f8a0f8fe1ba7ec9645fd8d3c
black-software100 <jt158161@gmail.com>
2023-08-04 16:58:12 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,45 +1,75 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.from(".student", {
+gsap.from(".name", { x: -100, duration: 4,color:"transparent"});
+gsap.from(".dev", { x: -100, duration: 4,color:"transparent"});
+// gsap.from(".about", { x: 100, duration: 4,color:"transparent"});
+
+gsap.from(".about", {
     scrollTrigger: {
-        trigger:"#student",
+        trigger:".about",
         start:"top 500px",
-        end:" top px",  
+        end:" top ",  
         scrub:1,
         // markers:true
     }, 
     ease:"none",
-    x: -1950,
-    duration:9
+    x: -190,
+    duration:1
 });
 
-gsap.from(".cardStudent",{
+gsap.from(".h-t",{
     scrollTrigger:{
-        trigger:"#student",
-        start:"top 500px",
-        end:" top 500px",  
+        trigger:".h-t",
+        start:"top center",
+        end:" top center",  
         scrub:1,
-        //markers:true
+        // markers:true
     },
-    background:"red",
-    rotation:250,
-    duration:1
+    color:"transparent",
+    duration:1,
+    x:450
 })
 
-gsap.from(".card-port",{
-    scrollTrigger:{
-        trigger:"#Portafolio",
-        start:"top 500px",
-        end:" top 500px",
-        scrub:1,
-    },
-    background:"transparent"
-})
+// gsap.from(".p-h",{
+//     scrollTrigger:{
+//         trigger:".p-h",
+//         start:"top center",
+//         end:" top center",  
+//         scrub:1,
+//         // markers:true
+//     },
+//     color:"transparent",
+//     duration:1,
+//     x:470
+// })
 
-gsap.from("#Arbol",{
-    scrollTrigger:{
-        trigger:"#Arbol",
-        // markers:true,
-        end:" top ", 
-        start:"top 100px",
-    }
-})
+// gsap.from(".a",{
+//     scrollTrigger:{
+//         trigger:".s-h",
+//         start:"top center",
+//         end:" top center",  
+//         scrub:1,
+//         markers:true
+//     },
+//     color:"transparent",
+//     duration:1,
+//     x:-450
+// })
+
+// gsap.from(".card-port",{
+//     scrollTrigger:{
+//         trigger:"#Portafolio",
+//         start:"top 500px",
+//         end:" top 500px",
+//         scrub:1,
+//     },
+//     background:"transparent"
+// })
+
+// gsap.from("#Arbol",{
+//     scrollTrigger:{
+//         trigger:"#Arbol",
+//         // markers:true,
+//         end:" top ", 
+//         start:"top 100px",
+//     }
+// })
