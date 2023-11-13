bcba56d745e3a854a63b269da4c88145e4fdc1df
ibarguen-dev <jt158161@gmail.com>
2023-11-13 16:47:48 -0500

    se organiza las extensiones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from '../component/Home.jsx'
+import Home from '../component/home.jsx'
 import Info from '../component/info.jsx'
 import Aplicaciones from './component/aplicaciones.jsx'
 import Skills from '../component/skills.jsx'
