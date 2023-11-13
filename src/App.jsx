96a49cc70c9379700f2214939583c9c891a7913d
ibarguen-dev <jt158161@gmail.com>
2023-11-13 16:34:11 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Navbar from './component/navbar'
+
 import Home from './component/Home'
 import Info from './component/info'
 import Aplicaciones from './component/aplicaciones'
 import Skills from './component/skills'
 import Estudios from './component/estudios'
+import Contactos from './component/contactos'
 import './css/App.css'
 
 function App() {
@@ -11,12 +12,12 @@ function App() {
 
   return (
     <main>
-      <Navbar/>
       <Home/>
       <Info/>
       <Aplicaciones/>
       <Skills/>
       <Estudios/>
+      <Contactos/>
     </main>
   )
 }
