3e55c8b14831da36f70ed22f3893ec41c6578c3d
ibarguen-dev <jt158161@gmail.com>
2023-11-13 17:15:47 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Contactos from "./Pages/contactos"
 import Home from "./Pages/home"
+import Aplicaciones from "./Pages/aplicaciones"
 
+
+import './css/App.css'
 function App() {
 
 
   return (
     <main>
       <Home/>
+
+      <Aplicaciones />
       <Contactos/>
     </main>
   )
