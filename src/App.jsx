a73526283b3636d9ead5e5c0f4bc5613a4aef003
ibarguen-dev <jt158161@gmail.com>
2023-11-13 17:08:58 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Contactos from "./Pages/contactos"
 import Home from "./Pages/home"
+
 function App() {
 
 
   return (
     <main>
       <Home/>
+      <Contactos/>
     </main>
   )
 }
---
+++ src/Pages/contactos.jsx
@@ -0,0 +1,27 @@
+import '../css/contactos.css'
+const Contactos = () => {
+
+    return (
+
+        <article className='contactos'>
+            <h1></h1>
+            <section className="contactos-section">
+                <a href="https://www.linkedin.com/in/juan-esteban-torres-ibarguen-0333b51b2/" target="blank">
+                    <i className="bi bi-linkedin"></i>
+                </a>
+                <a href="https://wa.me/573116992321" target="blank">
+                    <i className="bi bi-whatsapp"></i>                
+                </a>
+                <a href="https://github.com/ibarguen-dev" target="blank">
+                    <i className="bi bi-github"></i>
+                </a>
+            </section>
+        
+        </article>
+    )
+
+
+}
+
+
+export default Contactos
