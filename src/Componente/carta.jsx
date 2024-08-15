705e99283c98f9183ab3fb7b73f928493a18c7b4
ibarguen-dev <jt158161@gmail.com>
2024-08-14 22:41:27 -0500

    se mejora el porfolio

--- src/Componente/carta.jsx
+++ src/Componente/carta.jsx
@@ -1,30 +1,36 @@
-import PropTypes from 'prop-types';
-import '../css/carta.css'
-const Carta = ({img,title,info,buttonGithub,buttonWeb}) => {
-
-    return(
-        <article className='contenedor'>
-            <strong>{title}</strong>
-            <p>{info}</p>
-            <section className="contenedorCarta">
-                    <img src={img} alt={title} />
-            </section>
-            <section className='contenedorBotones'>
-                <a className='' href={buttonGithub} ><span><i className="bi bi-github"></i></span>Ver codigo fuente</a>
-                <a className='' href={buttonWeb}><span><i className="bi bi-globe"></i></span> Ir al sitio web</a>
-            </section>
-
-
-        </article>
-
-    )
-}
+import PropTypes from "prop-types";
+import "../css/carta.css";
+const Carta = ({ img, title, info, buttonGithub, buttonWeb }) => {
+  return (
+    <article className="contenedor">
+      <strong>{title}</strong>
+      <p>{info}</p>
+      <section className="contenedorCarta">
+        <img src={img} alt={title} />
+      </section>
+      <section className="contenedorBotones">
+        <a className="" href={buttonGithub}>
+          <span>
+            <i className="bi bi-github"></i>
+          </span>
+          Ver codigo fuente
+        </a>
+        <a className="" href={buttonWeb}>
+          <span>
+            <i className="bi bi-globe"></i>
+          </span>{" "}
+          Ir al sitio web
+        </a>
+      </section>
+    </article>
+  );
+};
 
 Carta.propTypes = {
-    img: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    info: PropTypes.string.isRequired,
-    buttonGithub: PropTypes.string.isRequired,
-    buttonWeb: PropTypes.string.isRequired,
-  };
-export default Carta
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  info: PropTypes.string.isRequired,
+  buttonGithub: PropTypes.string.isRequired,
+  buttonWeb: PropTypes.string.isRequired,
+};
+export default Carta;
--- src/js/const.js
+++ src/js/const.js
@@ -1,133 +1,151 @@
-
 export const apps = [
-    {
-        img:"https://firebasestorage.googleapis.com/v0/b/curriculum-vatea.appspot.com/o/npm.png?alt=media&token=f3bdea72-0b84-4ec7-b4b4-cf1425bdfd33",
-        title:"react router iba-dev",
-        info:"Es un componente basado en react-router, que permite en rutar las paginas estáticas de react.",
-        link:"https://www.npmjs.com/package/react-router-iba-dev",
-        buttonGithub:"https://github.com/ibarguen-dev/react-router-iba-dev",
-        buttonWeb:"https://www.npmjs.com/package/react-router-iba-dev"
-    },
-    {
-        img:"https://firebasestorage.googleapis.com/v0/b/curriculum-vatea.appspot.com/o/Hestia.webp?alt=media&token=1ce01670-9fc0-49b1-8690-ccf31672e3cf",
-        title:"Hestia",
-        info:"Hestia Es una aplicación creada en Python, que su principal fue es organizar archivos. También cuenta con varias opciones las cuales son Descargar videos y audios de YouTube, también tiene un convertidor de Word a PDF y viceversa.",
-        link:"https://github.com/ibarguen-dev/Hestia",
-        buttonGithub:"https://github.com/ibarguen-dev/Hestia",
-        buttonWeb:"https://github.com/ibarguen-dev/Hestia/releases"
-    },
-    {
-        img:"https://firebasestorage.googleapis.com/v0/b/curriculum-vatea.appspot.com/o/tres%20en%20raya.png?alt=media&token=8a605be8-6a69-4198-93c8-ef2daa7e0423",
-        title:"Tres en raya",
-        info:"Es el juego de tres en raya, creado en react. Su creación fue para la mejora de la lógica y el uso de useState y useEffect",
-        link:"https://tres-en-raya-swart.vercel.app/",
-        buttonGithub:"https://github.com/ibarguen-dev/Tres-en-raya",
-        buttonWeb:"https://tres-en-raya-6pinidcit-juantorres-dev.vercel.app/"
-    }
-
- ] 
-
- export const ConstSkill = [
-    {
-        name:"html",
-        level:"90",
-        porcentaje:{
-           "--clr":"#fc5f04",
-           "--num":"90" 
-        }
-    },
-    { 
-        name:"css",
-        level:"80",
-        porcentaje:{
-            "--clr":"#0478fc",
-            "--num":"80"
-        }
-    },
-    {
-        name:"js",
-        level:"70",
-        porcentaje:{
-            "--clr":"#fcf804",
-            "--num":"70"
-        }
-    },
-    {
-        name:"Express",
-        level:"85",
-        porcentaje:{
-            "--clr":"#04fc43",
-            "--num":"85"
-        }
-    },
-    {
-        name:"sql",
-        level:"85",
-        porcentaje:{
-            "--clr":"#0429fc",
-            "--num":"85"
-        }
-    },
-    {
-        name:"Mongo DB",
-        level:"85",
-        porcentaje: {
-            "--clr":"#024d14",
-            "--num":"85"
-        }
-    },
-    {
-        name:"php",
-        level:"85",
-        porcentaje:{
-            "--clr":"#20049e",
-            "--num":"85"
-        }
-    },
-    {
-        name:".net",
-        level:"70",
-        porcentaje: {
-            "--clr":"#7a049e",
-            "--num":"85"
-        }
-    },
-    {
-        name:"react",
-        level:"70",
-        porcentaje: {
-            "--clr":"#7af0ff",
-            "--num":"70"
-        }
-    },
-    {
-        name:"c++",
-        level:"10",
-        porcentaje: {
-            "--clr":"#035a66",
-            "--num":"10"
-        }
-    },
-    {
-        name:"python",
-        level:"85",
-        porcentaje:{
-            "--clr":"rgb(242, 255, 59)",
-            "--num":"85"
-        }
-    }
+  {
+    img: "/npm.webp",
+    title: "react router iba-dev",
+    info: "Es un componente basado en react-router, que permite en rutar las paginas estáticas de react.",
+    link: "https://www.npmjs.com/package/react-router-iba-dev",
+    buttonGithub: "https://github.com/ibarguen-dev/react-router-iba-dev",
+    buttonWeb: "https://www.npmjs.com/package/react-router-iba-dev",
+  },
+  {
+    img: "/Hestia.webp",
+    title: "Hestia",
+    info: "Hestia Es una aplicación creada en Python, que su principal fue es organizar archivos. También cuenta con varias opciones las cuales son Descargar videos y audios de YouTube, también tiene un convertidor de Word a PDF y viceversa.",
+    link: "https://github.com/ibarguen-dev/Hestia",
+    buttonGithub: "https://github.com/ibarguen-dev/Hestia",
+    buttonWeb: "https://github.com/ibarguen-dev/Hestia/releases",
+  },
+  {
+    img: "/tres en raya.webp",
+    title: "Tres en raya",
+    info: "Es el juego de tres en raya, creado en react. Su creación fue para la mejora de la lógica y el uso de useState y useEffect",
+    link: "https://tres-en-raya-swart.vercel.app/",
+    buttonGithub: "https://github.com/ibarguen-dev/Tres-en-raya",
+    buttonWeb: "https://tres-en-raya-6pinidcit-juantorres-dev.vercel.app/",
+  },
+  {
+    img: "/cutlinks.webp",
+    title: "Cutlinks",
+    info: "Cutlinks es un acortador de links como su nombre lo indica creado con angular y mongodb y utlizando tecnologias de incriptación",
+    link: "https://cutlinks-six.vercel.app/",
+    buttonGithub: "",
+    buttonWeb: "https://cutlinks-six.vercel.app/",
+  },
+  {
+    img: "/teachservice.webp",
+    title: "TeachService",
+    info: "Porfolio diseñado con las ultimas tecnologias ",
+    link: "https://portafolio-two-ecru.vercel.app/",
+    buttonGithub: "",
+    buttonWeb: "https://portafolio-two-ecru.vercel.app/",
+  },
 ];
 
+export const ConstSkill = [
+  {
+    name: "html",
+    level: "90",
+    porcentaje: {
+      "--clr": "#fc5f04",
+      "--num": "90",
+    },
+  },
+  {
+    name: "css",
+    level: "80",
+    porcentaje: {
+      "--clr": "#0478fc",
+      "--num": "80",
+    },
+  },
+  {
+    name: "js",
+    level: "70",
+    porcentaje: {
+      "--clr": "#fcf804",
+      "--num": "70",
+    },
+  },
+  {
+    name: "Express",
+    level: "85",
+    porcentaje: {
+      "--clr": "#04fc43",
+      "--num": "85",
+    },
+  },
+  {
+    name: "sql",
+    level: "85",
+    porcentaje: {
+      "--clr": "#0429fc",
+      "--num": "85",
+    },
+  },
+  {
+    name: "Mongo DB",
+    level: "85",
+    porcentaje: {
+      "--clr": "#024d14",
+      "--num": "85",
+    },
+  },
+  {
+    name: "php",
+    level: "85",
+    porcentaje: {
+      "--clr": "#20049e",
+      "--num": "85",
+    },
+  },
+  {
+    name: ".net",
+    level: "70",
+    porcentaje: {
+      "--clr": "#7a049e",
+      "--num": "85",
+    },
+  },
+  {
+    name: "react",
+    level: "70",
+    porcentaje: {
+      "--clr": "#7af0ff",
+      "--num": "70",
+    },
+  },
+  {
+    name: "c++",
+    level: "10",
+    porcentaje: {
+      "--clr": "#035a66",
+      "--num": "10",
+    },
+  },
+  {
+    name: "python",
+    level: "85",
+    porcentaje: {
+      "--clr": "rgb(242, 255, 59)",
+      "--num": "85",
+    },
+  },
+];
 
- export const ESTUDIOS = [
-    {
-        name:"Técnico en sistemas",
-        university:"Sena",
-        time:"Febrero:2020 - Marzo:2021"
-    },
-    {
-        name:"Técnico en desarrollo de software",
-        university:"Cesde",
-        time:"Agosto:2021 - septiembre:2023"
-    },
-]
+export const ESTUDIOS = [
+  {
+    name: "Técnico en sistemas",
+    university: "Sena",
+    time: "Febrero:2020 - Marzo:2021",
+  },
+  {
+    name: "Técnico en desarrollo de software",
+    university: "Cesde",
+    time: "Agosto:2021 - septiembre:2023",
+  },
+  {
+    name: "Técnologo en desarrollo de software",
+    university: "ITM",
+    time: "Agosto:2024 - Actualmente",
+  },
+];
